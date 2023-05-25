9523475a39bdcbc8650996d4546ec6df3c6673af
Kelly Shreeve <109543606+kellyshreeve@users.noreply.github.com>
2023-05-25 12:30:56 -0700

    dataset url and app.py dataset load

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-# TODO: sdkjlsdj 
-# ! This is an alert
-# ? This is a questions
-# * This is highlighted
+# Import Libraries
+import pandas as pd 
+import numpy as np
+import plotly.express as px
 
-my_dict = {a:1,
-           b:2,
-           }
+# Read in dataset
+url = 'https://raw.githubusercontent.com/kellyshreeve/Web-App-Project/main/ElectricCarData_Clean.csv?token=GHSAT0AAAAAACC2O2SU3UV3WOYKFXLLDT4KZDPW2XA'
+ev = pd.read_csv(url)
+
+# Rename columns with snake case
+ev = ev.rename(
+    columns={'Brand':'brand',
+             'Model':'model',
+             'AccelSec':'accel_sec',
+             'TopSpeed_KmH':'top_speed_kmh',
+             'Range_Km':'range_km',
+             'Efficiency_WhKm':'efficiency_whkm',
+             'FastCharge_KmH':'fast_charge_kmh',
+             'RapidCharge':'rapid_charge',
+             'PowerTrain':'power_train',
+             'PlugType':'plug_type',
+             'BodyStyle':'body_style',
+             'Segment':'segment',
+             'Seats':'seats',
+             'PriceEuro':'price_euro'
+             }
+)
+
+# Clean duplicates and missing values
+ev['fast_charge_kmh'] = ev['fast_charge_kmh'].replace('-', np.NaN) # Change '-' to NaN
+
+ev['fast_charge_kmh'] = pd.to_numeric(ev['fast_charge_kmh']) # Change fast_charge_kmh to int type data
+
+ev = ev.drop_duplicates(subset=['brand', 'model']).reset_index(drop=True)
+
