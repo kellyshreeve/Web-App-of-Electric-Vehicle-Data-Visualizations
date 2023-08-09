a27e9316624c617f59a40733c54b599aba3d88f6
Kelly Shreeve <109543606+kellyshreeve@users.noreply.github.com>
2023-08-09 13:23:24 -0700

    Update Title

--- app.py
+++ app.py
@@ -51,7 +51,6 @@ top_brands = ev[(ev['brand']=='Tesla ') | (ev['brand']=='Audi ') | (ev['brand']=
 
 ## CREATE HEADER
 st.header('Electric Vehicle Market Visualizations')
-st.subheader('By Brand, Body Style, and Price')
 
 # Create Text
 st.markdown('Welcome to the electric vehicle data visualization page! Click the buttons \nand toggle the colors to see how price, efficiency, and range of electric vehicles compare across the market.')
